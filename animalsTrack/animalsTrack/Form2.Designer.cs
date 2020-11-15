05e0f46654158fc0943f223638bada139eefc1e3
Xie,you-leng <yoyo881016@gmail.com>
2020-11-15 23:16:05 +0800

    1. push project to nas

---
+++ animalsTrack/animalsTrack/Form2.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace animalsTrack
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Btn_changeFar = new System.Windows.Forms.Button();
+            this.Pic_track = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_track)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Btn_changeFar
+            // 
+            this.Btn_changeFar.BackColor = System.Drawing.SystemColors.Control;
+            this.Btn_changeFar.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Btn_changeFar.Location = new System.Drawing.Point(890, 495);
+            this.Btn_changeFar.Name = "Btn_changeFar";
+            this.Btn_changeFar.Size = new System.Drawing.Size(130, 35);
+            this.Btn_changeFar.TabIndex = 0;
+            this.Btn_changeFar.Text = "切換遠端模式";
+            this.Btn_changeFar.UseVisualStyleBackColor = false;
+            this.Btn_changeFar.Click += new System.EventHandler(this.Btn_changeFar_Click);
+            // 
+            // Pic_track
+            // 
+            this.Pic_track.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Pic_track.Location = new System.Drawing.Point(20, 20);
+            this.Pic_track.Margin = new System.Windows.Forms.Padding(2);
+            this.Pic_track.Name = "Pic_track";
+            this.Pic_track.Size = new System.Drawing.Size(510, 510);
+            this.Pic_track.TabIndex = 2;
+            this.Pic_track.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(557, 20);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(304, 43);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(911, 138);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(924, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1050, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(557, 69);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(327, 461);
+            this.textBox4.TabIndex = 8;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 551);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Pic_track);
+            this.Controls.Add(this.Btn_changeFar);
+            this.Name = "Form2";
+            this.Text = "可彈性擴充之動物行為量測系統-PC端模式-動物實驗結果";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_track)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_changeFar;
+        public System.Windows.Forms.PictureBox Pic_track;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
---
+++ animalsTrack/animalsTrack/Form2.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace animalsTrack
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        private SerialPort comport = new SerialPort();
+        //private int totalLength = 0;
+        //delegate void Display(byte[] buffer);
+        bool rs232Key;
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.button1.Text = "Open COM";
+
+                this.button1.Click += new EventHandler(Rs232PortDoor);
+
+                rs232Key = true;
+
+                //comport = new SerialPort("COM5", 115200, Parity.None, 8, StopBits.One);
+                //comport.ReadTimeout = 100;
+
+                //comport.Open();
+                //if (!comport.IsOpen)
+                //{
+                //    MessageBox.Show("埠開啟失敗");
+                //    return;
+                //}
+                //else
+                //{
+                //    textBox1.AppendText("埠開啟成功\r\n");
+                //}
+                //comport.DataReceived += new SerialDataReceivedEventHandler(comport_DataReceived);
+            }
+            catch (Exception ex)
+            {
+                comport.Dispose();
+                textBox1.AppendText(ex.Message);
+            }
+        }
+
+        private void Rs232PortDoor(object sender, EventArgs e)
+        {
+            if (rs232Key)
+            {
+                this.button1.Text = "Close Port";
+
+                // 設定使用的 PORT
+                this.comport.PortName = "COM5";
+
+                // 檢查 PORT 是否關閉
+                if (!comport.IsOpen)
+                    this.comport.Close();
+
+                // 初始化 PORT
+                this.comport.BaudRate = 115200;            // baud rate = 9600
+                this.comport.Parity = Parity.None;       // Parity = none
+                this.comport.StopBits = StopBits.One;    // stop bits = one
+                this.comport.DataBits = 8;               // data bits = 8
+
+                // 設定 PORT 接收事件
+                comport.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);
+
+                // 打開 PORT
+                comport.Open();
+
+                // 清空 serial port 的緩存
+                comport.DiscardInBuffer();       // RX
+                comport.DiscardOutBuffer();      // TX
+
+                rs232Key = false;
+            }
+            else
+            {
+                this.button1.Text = "Open Port";
+
+                // 清空 serial port 的緩存
+                comport.DiscardInBuffer();       // RX
+                comport.DiscardOutBuffer();      // TX
+
+                // 關閉 PORT
+                this.comport.Close();
+
+                rs232Key = true;
+            }
+        }
+        void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            byte RB = Byte.Parse(comport.ReadByte().ToString());
+            //SetText(RB.ToString());
+            SetText(textBox4.Text + RB.ToString() + " ");
+        }
+
+        delegate void SetTextCallback(string text);
+        private void SetText(string text)
+        {
+            if (this.textBox4.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.textBox4.Text = text;
+            }
+        }
+
+        //private void comport_DataReceived(Object sender, SerialDataReceivedEventArgs e)
+        //{
+        //    Byte[] buffer = new Byte[1024];
+        //    Int32 length = (sender as SerialPort).Read(buffer, 0, buffer.Length);
+        //    Array.Resize(ref buffer, length);
+        //    Display d = new Display(DisplayText);
+        //    this.Invoke(d, new Object[] { buffer });
+        //}
+
+        //private void DisplayText(Byte[] buffer)
+        //{
+        //    textBox1.Text += String.Format("{0}{1}", BitConverter.ToString(buffer), Environment.NewLine);
+        //    totalLength = totalLength + buffer.Length;
+        //    label1.Text = totalLength.ToString();
+        //}
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox4.Clear();
+        }
+
+        //切換遠端
+        private void Btn_changeFar_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+    }
+}
